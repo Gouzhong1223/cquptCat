391de268e650e265683428e36ea037ca7a11f26e
Gouzhong <1162864960@qq.com>
2020-04-18 18:22:53 +0800

    添加一下实体类以及配置文件

---
+++ src/main/java/org/gouzhong1223/cymmtj/mapper/CatMapper.java
@@ -0,0 +1,28 @@
+package org.gouzhong1223.cymmtj.mapper;
+
+import org.gouzhong1223.cymmtj.pojo.Cat;
+
+/**
+ * @Author : Gouzhong
+ * @Blog : www.gouzhong1223.com
+ * @Description : 
+ * @Date : create by QingSong in 2020-04-18 6:18 下午
+ * @Email : gouzhong1223@gmail.com
+ * @Since : JDK 1.8
+ * @PackageName : org.gouzhong1223.cymmtj.mapper
+ * @ProjectName : cymmtj
+ * @Version : 1.0.0
+ */
+public interface CatMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Cat record);
+
+    int insertSelective(Cat record);
+
+    Cat selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Cat record);
+
+    int updateByPrimaryKey(Cat record);
+}
---
+++ src/main/java/org/gouzhong1223/cymmtj/pojo/Cat.java
@@ -0,0 +1,86 @@
+package org.gouzhong1223.cymmtj.pojo;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @Author : Gouzhong
+ * @Blog : www.gouzhong1223.com
+ * @Description : Cat 实体类
+ * @Date : create by QingSong in 2020-04-18 6:18 下午
+ * @Email : gouzhong1223@gmail.com
+ * @Since : JDK 1.8
+ * @PackageName : org.gouzhong1223.cymmtj.pojo
+ * @ProjectName : cymmtj
+ * @Version : 1.0.0
+ */
+@ApiModel(value = "org-gouzhong1223-cymmtj-pojo-Cat")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Cat implements Serializable {
+    /**
+     * 猫咪主键
+     */
+    @ApiModelProperty(value = "猫咪主键")
+    private Integer id;
+
+    /**
+     * 猫咪名字
+     */
+    @ApiModelProperty(value = "猫咪名字")
+    private String name;
+
+    /**
+     * 猫咪所属楼栋
+     */
+    @ApiModelProperty(value = "猫咪所属楼栋")
+    private String ban;
+
+    /**
+     * 猫咪毛色
+     */
+    @ApiModelProperty(value = "猫咪毛色")
+    private String color;
+
+    /**
+     * 猫咪性别
+     */
+    @ApiModelProperty(value = "猫咪性别")
+    private String sex;
+
+    /**
+     * 猫咪外貌
+     */
+    @ApiModelProperty(value = "猫咪外貌")
+    private String foreignTrade;
+
+    /**
+     * 猫咪性格
+     */
+    @ApiModelProperty(value = "猫咪性格")
+    private String character;
+
+    /**
+     * 更新时间
+     */
+    @ApiModelProperty(value = "更新时间")
+    private LocalDateTime updateTime;
+
+    /**
+     * 猫咪所属分类
+     */
+    @ApiModelProperty(value = "猫咪所属分类")
+    private String type;
+
+    private static final long serialVersionUID = 1L;
+}
