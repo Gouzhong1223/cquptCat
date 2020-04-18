c3cfd28af84b35257357f38c02e9acf4905c8c2d
Gouzhong <1162864960@qq.com>
2020-04-18 17:39:04 +0800

    Init Project

---
+++ src/main/java/org/gouzhong1223/cymmtj/package-info.java
@@ -0,0 +1,28 @@
+/*
+ *              Copyright 2020 By Gouzhong1223
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+/**
+ * @Author : Gouzhong
+ * @Blog : www.gouzhong1223.com
+ * @Description : 重游猫咪图鉴小程序后端服务以及管理端后端服务
+ * @Date : create by QingSong in 2020-04-18 5:35 下午
+ * @Email : gouzhong1223@gmail.com
+ * @Since : JDK 1.8
+ * @PackageName : org.gouzhong1223.cymmtj
+ * @ProjectName : cymmtj
+ * @Version : 1.0.0
+ */
+package org.gouzhong1223.cymmtj;
---
+++ src/test/java/org/gouzhong1223/cymmtj/CymmtjApplicationTests.java
@@ -0,0 +1,13 @@
+package org.gouzhong1223.cymmtj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CymmtjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
